ff4f76e15ab74a79f9aa4d5908c1102d21547975
radif420 <jahirradif@gmail.com>
2025-07-24 23:49:16 +0600

    Update page.tsx

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,5 +1,6 @@
-import { supabase } from '@/lib/supabaseClient';
 'use client';
+
+import { supabase } from '@/lib/supabaseClient';
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
